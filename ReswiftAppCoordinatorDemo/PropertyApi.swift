ceee6c9c9e7d3bc17c8b0d93cdd2b9b6c92ac371
Liu Xianning <xianlinbox@gmail.com>
2017-01-04 17:32:40 +0800

    add Alamofire

--- ReswiftAppCoordinatorDemo/PropertyApi.swift
+++ ReswiftAppCoordinatorDemo/PropertyApi.swift
@@ -5,6 +5,7 @@
 //  Created by Xianning Liu on 04/01/2017.
 //  Copyright © 2017 xianlinbox. All rights reserved.
 //
+import Alamofire
 
 struct PropertyApi {
     func findProperties(placeName:String) -> String{
